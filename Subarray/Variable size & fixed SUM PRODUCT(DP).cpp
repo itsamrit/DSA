eef1a6628042dd9c93fb156a54ac296b03ee4735
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-10-15 09:29:21 +0530

    Update Variable size & fixed SUM PRODUCT(DP).cpp

--- Subarray/Variable size & fixed SUM PRODUCT(DP).cpp
+++ Subarray/Variable size & fixed SUM PRODUCT(DP).cpp
@@ -1,7 +1,8 @@
 // ✅✅If subarray can become valid(according to problem) again by adding more characters ,i,e, we cant use 2 pointer(sliding window) i,e cant move left pointer as addition of more charaters can make it valid again
 // ✅ eg : target =4 and sliding window got sum 6 but still we cant move left pointer since a negative no may come -2 and sum become from current i to -2 as 4
 // ✅Except SUM QUESTIONS AND VARIABLE SLIING WINDOW are takes o(n^2) time
-
+// ✅✅✅IF ASKED VARIABLE SIZE AND SEMI VARIABLE i,e greater than or smaller than given value with all positive elements in array then use sliding window VARIABLE SUBARRAY  
+//        eg: Smallest subarray with sum greater than or equal to x
 Tc=o(nlogn)
 // If asked you need to select k element from left end or right end of array such that its sum is max then ans= total sum of array - subarray with min sum of k elements
 // This works for both pos and neg while sliding works only for positive
