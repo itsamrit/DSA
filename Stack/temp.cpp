93ba7d4adad69587174a72b160c8d5ca79fc0464
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-10-19 15:17:10 +0530

    Create temp.cpp

---
+++ Stack/temp.cpp
@@ -0,0 +1,10 @@
+palylist of youtube stack
+
+
+?? ???stacks question defintiin + similar to code is similar to variable size subarray but (THERE iS no window)in stack we dont make 2 pointer we start iterating from current lement to already iterated but not rejected no window
+
+lexographically maximum no subset more than 10 questions with different just remember it is solved by stack
+
+BUT once rejected/deleted element for current is then rejected for all other element IN NGR element which is not next greater than current cant be next greater for the rest behind it
+
+New question solve in leetcode and make notes there only
