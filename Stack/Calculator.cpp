36790d35d91dc6c3a8c2b8e983bb4cde03ca1626
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-11-02 09:51:41 +0530

    Update Calculator.cpp

--- Stack/Calculator.cpp
+++ Stack/Calculator.cpp
@@ -1,1 +1,3 @@
 you will be given either +-*/ or +-() questions . You cant have */ and () combined and solved using stack
+Like +- have same priority so as */ have same priority.eg: 2*3/2 .U either divide first or multiply ans will be same
+
