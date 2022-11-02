e8559d66f0c969419ac2181c9a0514488f039a54
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-11-02 18:04:24 +0530

    Update Calculator.cpp

--- Stack/Calculator.cpp
+++ Stack/Calculator.cpp
@@ -3,4 +3,5 @@ Like +- have same priority so as */ have same priority.eg: 2*3/2 .U either divid
 
 IN +-*/ whenever +- comes we simply push it into stack & whenever */ comes we go back or pop 1 element and push the current element multiplied or divided by poped element.
    At last we add all elemetns of stack.
-In +-() push all element in stack and when ) comes we and add till ( appers
+In +-() push all element in stack and when ) comes we go back and add each element of stack till ( appers. At last we can add all elements of stack.
+   Improvement : We can add each element while traversing and only push sumation of all element before ( appears & pop that top most sumation when ) appears & add it to current sum.
