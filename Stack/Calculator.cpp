98969119ea313e54d99175453f6c6bea9a5157ee
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-11-02 15:43:36 +0530

    Update Calculator.cpp

--- Stack/Calculator.cpp
+++ Stack/Calculator.cpp
@@ -1,3 +1,6 @@
 you will be given either +-*/ or +-() questions . You cant have */ and () combined and solved using stack
 Like +- have same priority so as */ have same priority.eg: 2*3/2 .U either divide first or multiply ans will be same
 
+IN +-*/ whenever +- comes we simply push it into stack & whenever */ comes we go back or pop 1 element and push the current element multiplied or divided by poped element.
+   At last we add all elemetns of stack.
+In +-() push all element in stack and when ) comes we and add till ( appers
