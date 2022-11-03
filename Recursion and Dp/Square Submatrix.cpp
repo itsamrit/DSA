6fc4be8ae8740241f8e23c46d8e690c74e1c7c9c
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-11-03 18:36:22 +0530

    Update Square Submatrix.cpp

--- Recursion and Dp/Square Submatrix.cpp
+++ Recursion and Dp/Square Submatrix.cpp
@@ -6,11 +6,11 @@ int countSquares(vector<vector<int>>& matrix) {
 		for(int i=0;i<matrix.size();++i)
                     for(int j=0;j<matrix[0].size();++j)
 		        if(i!=0 && j!=0 && matrix[i][j]==1)  
-			    matrix[i][j] = 1 + min(matrix[i-1][j],min(matrix[i][j-1],matrix[i-1][j-1]));  //If this index is 1 then max length of square matrix formed starting from this index (in part of matrix we have traversed i,e upward left part considering cur index to centre of graph paper) 
-                            // Max length of square matrix = max squares asscoiated with cur index (in upward left part considering this is centre of graph paper)
+			    matrix[i][j] = 1 + min(matrix[i-1][j],min(matrix[i][j-1],matrix[i-1][j-1]));  //✅Max length of square matrix from this index  ✅(in part of matrix we have traversed i,e upward left part considering cur index to centre of graph paper) 
+                            //✅Max length of square matrix = max squares asscoiated with cur index  (in upward left part considering this is centre of graph paper)
 			
 	                 count +=matrix[i][j];   // Add all squares associated with each index
 	                 //For max area square submatrix with all ones : ans = max(matrix[i][j], ans);
-        
+       .
 		return count;
 }
