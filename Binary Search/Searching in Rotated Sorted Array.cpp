af449796473e2659d368ae2a1173cf4ee2746a7f
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-11-09 15:54:23 +0530

    Update Searching in Rotated Sorted Array.cpp

--- Binary Search/Searching in Rotated Sorted Array.cpp
+++ Binary Search/Searching in Rotated Sorted Array.cpp
@@ -2,10 +2,11 @@ int getPivot(vector<int>& nums, int n){
         int s=0;
         int e =nums.size()-1;
         // ✅✅ WE ONLY COMPARE MID AND RIGHTMOST ELEMENT NO USE OF LEFT element
+        
         while(s<e){
             int m = s+(e-s)/2;
-            if(nums[m] > nums[e]) s=m+1;          // ✅left side has big values (rotated array)
-            else if(nums[m]<nums[s])e=m;                        // right side has big value (not rotated)
+            if(nums[m] > nums[e]) s=m+1;  // ✅left side has big values.✅TLE : INCREMENT MID SINCE nums[mid]>nums[e],so it is fixed that mid is not pivot. 
+            else if(nums[m]<nums[s])e=m;           // right side has big value ✅U cant decrement e=m-1; Since it is mid can be pivot.
             else e--;
         }
         return s;
