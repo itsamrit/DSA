13542325db8505e0ddf99b653b31c537d1b2cf75
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-11-18 19:29:19 +0530

    Create Greedy i,e Best option at each time.cpp

---
+++ PriorityQueue/Greedy i,e Best option at each time.cpp
@@ -0,0 +1,8 @@
+We take best option at each time(unlike recursion which considers all options) & store it in priority_queue.
+IF boundry condition is broken we pop out either most irrerelevant or most relevant things according to problem.
+  // LIKE DIJKSTRA
+// 🫡In this type of question anything can be answer either pq.size() or no of times you popped out. So figure out in each question
+  // NO FORMAT FOR THIS QUESTIONS
+  eg: In course schedule : If take all element inside our (maxheap)queue & if sum of all element of queue becomes more than deadline we pop out course which is taking max time.
+    
+      
