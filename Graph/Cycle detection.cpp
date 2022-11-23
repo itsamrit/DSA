74a37ec2eb99d1260da60facb922bbdccb981037
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-11-23 17:48:49 +0530

    Update Cycle detection.cpp

--- Graph/Cycle detection.cpp
+++ Graph/Cycle detection.cpp
@@ -30,29 +30,3 @@ bool isCycle(int N, vector<int> adj[]) {
       return false;
 }
 
-
-Undirected Cycle BFS :
-bool cycle;
-
-void bfs(int s){
-    queue<pair<int,int>>q;   // For storing current node c & its parent node p
-    vis[s]=1;
-    q.push({s, -1});    // In bfs queue intialized first unlike dfs
-     
-    while(q.size()) {  
-        int c = q.front().first;
-        int p = q.front().second;
-        q.pop();  
-     
-        for(auto i:g[c]){  
-            if (vis[i]==0) {  
-                vis[i]=1;   // In bfs vis[i] is marked here unlike dfs
-                q.push({i,c});
-            }
-            else if(p!=i){
-                cycle=true;
-                return;
-            }
-        }
-    }
-}
