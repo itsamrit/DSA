a03141535dba0e75ba4126d9a5b1296011486b72
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-11-26 15:34:57 +0530

    Update Searching in Rotated Sorted Array.cpp

--- Binary Search/Searching in Rotated Sorted Array.cpp
+++ Binary Search/Searching in Rotated Sorted Array.cpp
@@ -1,36 +1,39 @@
-int getPivot(vector<int>& nums, int n){
-        int s=0;
-        int e =nums.size()-1;
-        // ✅✅ WE ONLY COMPARE MID AND RIGHTMOST ELEMENT NO USE OF LEFT element
-        
-        while(s<e){
-            int m = s+(e-s)/2;
-            if(nums[m] > nums[e]) s=m+1;  // ✅left side has big values.✅TLE : INCREMENT MID SINCE nums[mid]>nums[e],so it is fixed that mid is not pivot. 
-            else if(nums[m]<nums[s])e=m;           // right side has big value ✅U cant decrement e=m-1; Since it is mid, it can be pivot.
-            else e--;                    //✅ For upper bound e++; when both found equal
-        }
-        return s;
-}
-    
-int binarySearch(vector<int>& nums, int s, int e, int target){  //Apply to that part binary search where there is possibility
-        int m;
-        while(s <= e){
-            m = s + (e-s)/2;
-            if(nums[m]== target) return m;
-            else if(nums[m]< target) s = m+1;
-            else if(nums[m]>target) e = m-1;
-            else e--;
+int bs(vector<int>&nums,int target,int s,int e){
+        while(s<=e){
+            int mid=(s+(e-s)/2);
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
         }
         return -1;
-}
-    
-int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int pivot = getPivot(nums, n);
-        if(target >= nums[pivot] && target <= nums[n-1]){
-            return binarySearch(nums, pivot, n-1, target);
+    }
+    int search(vector<int>& nums, int target) {
+        int left=0,right=nums.size()-1;
+        int mid;
+        int ind=-1;
+        while(left<=right){
+            mid=left+(right-left)/2;
+            if((mid==0 || nums[mid]<nums[mid-1]) && (mid==nums.size()-1 || nums[mid]<nums[mid+1])){
+                ind=mid;
+                break;
+            }
+            else if(nums[mid]>nums[right]){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
         }
-        else{
-            return binarySearch(nums, 0, pivot-1, target);
-        }
-}
+        
+        int f=bs(nums,target,0,ind-1);        //🟢Sorted 0 to ind-1 .Since nums[ind] = smallest in rotated array. So dont do 0 to ind
+        int s=bs(nums,target,ind,nums.size()-1);
+        if(f!=-1)return f;
+        else if(s!=-1)return s;
+        else return -1;
+    }
