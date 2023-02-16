3b11881d3d39aa90d1af77396546abb5b711f3e5
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2023-02-17 02:42:03 +0530

    Update Sort Heap.cpp

--- Array Sorting/Sort Heap.cpp
+++ Array Sorting/Sort Heap.cpp
@@ -16,7 +16,7 @@ vector<int> sort(vector<int>arr,int k){
 }
 
 
-Greedy : Further building you can reach with k ladders(u can reach any height) and m bricks.1st push for(i=0 to k) push all elements in pq and assume all climed by ladder then insert for(i=k to n) remove all min element so that they are climed by bricks are limited. If bricks are over this is the building where we can reach.
+Greedy : Further building you can reach with k ladders and m bricks.By using ladder u can go to any height.1st push for(i=0 to k) push all elements in pq and assume all climed by ladder then insert for(i=k to n) remove all min element so that they are climed by bricks are limited. If bricks are over this is the building where we can reach.
 Print k closest numbers to given number: priority_queue<pair<int,int>>  and  maxheap.push({abs(arr[i]-x),arr[i]});
 Top k most frequent element Frequency sort: map[i]++;  minheap.push({i->second,i->first});
 Print k closest points from origin:  priority_queue<pair<int, pair<int,int>>maxheap;    maxheap.push({arr[i][0]*arr[i][0]+arr[i][1]*arr[i][1] , {arr[i][0],arr[i][1]}};   //dis of point (x,y) from (0,0)=root(x^2 +y^2)
