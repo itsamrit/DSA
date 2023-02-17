a854238230d5d743107ea1b01648838314ee96c9
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2023-02-17 18:02:59 +0530

    Update Calculator.cpp

--- Stack/Calculator.cpp
+++ Stack/Calculator.cpp
@@ -1,7 +1,7 @@
 you will be given either +-/* or +-() questions . You cant have */ and () combined and solved using stack
-Like +- have same priority so as */ have same priority.eg: 2*3/2 .U either divide first or multiply ans will be same
+Like +- have same priority so as /* have same priority.eg: 2*3/2 .U either divide first or multiply ans will be same
 
-IN +-*/ whenever +- comes we simply push it into stack & whenever */ comes we go back or pop 1 element and push the current element multiplied or divided by poped element.
+IN +-*/ whenever +- comes we simply push it into stack & whenever /* comes we go back or pop 1 element and push the current element multiplied or divided by poped element.
    At last we add all elemetns of stack.
 In +-() push all element in stack and when ) comes we go back and add each element of stack till ( appers. At last we can add all elements of stack.
    Improvement : We can add each element while traversing and only push sumation of all element before ( appears & pop that top most sumation when ) appears & add it to current sum.
