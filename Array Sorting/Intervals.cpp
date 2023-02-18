4e82f2e65dc444e9f568746f5b07a7261d335790
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2023-02-18 12:44:59 +0530

    Update Intervals.cpp

--- Array Sorting/Intervals.cpp
+++ Array Sorting/Intervals.cpp
@@ -1,1 +1,2 @@
 Sort the interval based on requirement either by starting index v[i][0] or ending index v[i][1]
+Create another ans vector to insert the intervals.Dont modify existing array.If ans coming you can optimize space complex by modifying
