6a00da9c87fe9961e0aeef434dd4b476bde7a52e
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2023-02-20 17:42:36 +0530

    Update ⭐DP LCA.cpp

--- Tree/⭐DP LCA.cpp
+++ Tree/⭐DP LCA.cpp
@@ -1,12 +1,12 @@
     TreeNode* dfs(TreeNode*root,TreeNode*p, TreeNode*q){
         if(!root)return NULL;
-        if(root==p || root==q)return root;
+        if(root==p || root==q)return root;   //🟩considering p to be root.IF p is intial root then other must be some child or grandchild of it.so ans will be root
         
         TreeNode* left=dfs(root->left,p,q);
         TreeNode* right=dfs(root->right,p,q);
         if(left && right)return root;
-        if(left)return left;
-        if(right)return right;
+        if(left)return left;            //🟩considering p to be root.IF p is intial root then other must be some child or grandchild of it.so ans will be root
+        if(right)return right;        
         return NULL;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
