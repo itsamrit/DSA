2cd6dad58943fa8aa7231322ceb7dbdb89e675a8
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2023-02-27 18:34:56 +0530

    Update Intervals.cpp

--- Array Sorting/Intervals.cpp
+++ Array Sorting/Intervals.cpp
@@ -1,2 +1,5 @@
+🟩🟩Only rememmber code of merging intervals .
+  If problem of inserting intervals and array is already sorted then insertt the interval using insert() fun on right place based on itering & mainitain sorted array with first element
+  Merge the intervals  starting from interval where you inserted .Same tc=0(n/2 inserting) & 0(n/2merging) =0(n)
 Sort the interval based on requirement either by starting index v[i][0] or ending index v[i][1]
 Create another ans vector to insert the intervals.Dont modify existing array.If ans coming you can optimize space complex by modifying
