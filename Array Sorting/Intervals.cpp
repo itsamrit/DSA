1e3bb138ae45106c946e6d60704aa21e74721283
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2023-02-27 20:59:02 +0530

    Update Intervals.cpp

--- Array Sorting/Intervals.cpp
+++ Array Sorting/Intervals.cpp
@@ -4,3 +4,33 @@
   
 Sort the interval based on requirement either by starting index v[i][0] or ending index v[i][1] 
 Create another ans vector to insert the intervals.Dont modify existing array.If ans coming you can optimize space complex by modifying
+
+
+    vector<vector<int>> insert(vector<vector<int>>& in, vector<int>& neww) {
+    🟩Code to insert
+//         int n=in.size();
+//         for(int i=0;i<in.size();i++){
+//             if(in[i][0]>=neww[0]){
+//                 in.insert(in.begin()+i,neww);
+//                 break;
+//             }
+//         }
+//         if(in.size()==0)return {neww};
+//         if(n==in.size())in.push_back(neww);
+        
+        🟩Code to merge
+        vector<vector<int>>ans;
+        int i=0,k=-1;
+        while(i<in.size()){
+            if(ans.size() && ans[k][1]>=in[i][0]){  //🟩🟩Always compare with ans[k] index not with previous interval like in[i-1]>=in[i]..
+                ans[k][0]=min(ans[k][0],in[i][0]);
+                ans[k][1]=max(ans[k][1],in[i][1]);
+            }
+            else{
+                ans.push_back(in[i]);
+                k++;
+            }
+            i++;
+        }
+        return ans;
+    }
