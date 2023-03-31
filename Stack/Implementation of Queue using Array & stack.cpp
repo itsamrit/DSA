2f1d53db203d2cfc3076fc6a28f4f5c2d1524ca1
Amrit Rai <amritrai9140@gmail.com>
2023-03-31 11:04:20 +0530

    Update Implementation of Queue using Array & stack.cpp

--- Stack/Implementation of Queue using Array & stack.cpp
+++ Stack/Implementation of Queue using Array & stack.cpp
@@ -1,1 +1,112 @@
+//LINk https://takeuforward.org/data-structure/implement-queue-using-array/
 
+// Time Complexity:  pop function: O(1)   push function: O(1) top function: O(1) size function: O(1)
+
+// Space Complexity: Whole Queue: O(n)
+
+class Queue {
+  int * arr;
+  int start, end, currSize, maxSize;
+  public:
+    Queue() {
+      arr = new int[16];
+      start = -1;
+      end = -1;
+      currSize = 0;
+    }
+
+  Queue(int maxSize) {
+    ( * this).maxSize = maxSize;
+    arr = new int[maxSize];
+    start = -1;
+    end = -1;
+    currSize = 0;
+  }
+  void push(int newElement) {
+    if (currSize == maxSize) {
+      cout << "Queue is full\nExiting..." << endl;
+      exit(1);
+    }
+    if (end == -1) {
+      start = 0;
+      end = 0;
+    } else
+      end = (end + 1) % maxSize;
+    arr[end] = newElement;
+    cout << "The element pushed is " << newElement << endl;
+    currSize++;
+  }
+  int pop() {
+    if (start == -1) {
+      cout << "Queue Empty\nExiting..." << endl;
+    }
+    int popped = arr[start];
+    if (currSize == 1) {
+      start = -1;
+      end = -1;
+    } else
+      start = (start + 1) % maxSize;
+    currSize--;
+    return popped;
+  }
+  int top() {
+    if (start == -1) {
+      cout << "Queue is Empty" << endl;
+      exit(1);
+    }
+    return arr[start];
+  }
+  int size() {
+    return currSize;
+  }
+};
+
+
+
+// USING 1 STACK :
+//push o(n) pop & top o(1)
+//Using 2 stack push o(1) while pop & top o(n)
+// LINK :https://takeuforward.org/data-structure/implement-queue-using-stack/
+//1stack implementation :
+struct Queue {
+  stack < int > input, output;
+  
+  // Push elements in queue
+  void Push(int data) {
+    // Pop out all elements from the stack input
+    while (!input.empty()) {
+      output.push(input.top());
+      input.pop();
+    }
+    // Insert the desired element in the stack input
+    cout << "The element pushed is " << data << endl;
+    input.push(data);
+    // Pop out elements from the stack output and push them into the stack input
+    while (!output.empty()) {
+      input.push(output.top());
+      output.pop();
+    }
+  }
+  // Pop the element from the Queue
+  int Pop() {
+    if (input.empty()) {
+      cout << "Stack is empty";
+      exit(0);
+    }
+    int val = input.top();
+    input.pop();
+    return val;
+  }
+  // Return the Topmost element from the Queue
+  int Top() {
+    if (input.empty()) {
+      cout << "Stack is empty";
+      exit(0);
+    }
+    return input.top();
+  }
+  // Return the size of the Queue
+  int size() {
+    return input.size();
+  }
+};
