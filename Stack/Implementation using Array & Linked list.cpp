9e9e0363d387ffbb9ccd8fca623bda930ea6fbe3
Amrit Rai <amritrai9140@gmail.com>
2023-03-31 10:40:21 +0530

    Create Implementation using Array & Linked list.cpp

---
+++ Stack/Implementation using Array & Linked list.cpp
@@ -0,0 +1,35 @@
+class Stack {
+  int size;
+  int * arr;
+  int top;
+  public:
+    Stack() {
+      top = -1;
+      size = 1000;
+      arr = new int[size];
+    }
+  void push(int x) {
+    top++;
+    arr[top] = x;
+  }
+  int pop() {
+    int x = arr[top];
+    top--;
+    return x;
+  }
+  int Top() {
+    return arr[top];
+  }
+  int Size() {
+    return top + 1;
+  }
+};
+
+int main() {
+
+  Stack s;
+  s.push(6);
+  s.push(3);
+  s.push(7);
+  cout<<s.Size();
+}
