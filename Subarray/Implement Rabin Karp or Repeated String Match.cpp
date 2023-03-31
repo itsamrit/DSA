001069f3408a196e21b6df646ec948d052097b11
Amrit Rai <amritrai9140@gmail.com>
2023-03-31 19:05:56 +0530

    Create Implement Rabin Karp or Repeated String Match.cpp

---
+++ Subarray/Implement Rabin Karp or Repeated String Match.cpp
@@ -0,0 +1,11 @@
+int repeatedStringMatch(string a, string b) {
+        string org=a;
+        while(a.length()<b.length()){
+            a+=org;
+        }
+        if(a.find(b)!=string::npos)return a.length()/org.length();
+        a+=org;
+        if(a.find(b)!=string::npos)return a.length()/org.length();
+        return -1;    
+        
+}
