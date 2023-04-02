e002a29bb5aaef932f9bdcb11f9d56c17ccc3a1d
Amrit Rai <amritrai9140@gmail.com>
2023-04-02 15:06:24 +0530

    Update Construct Tree from inorder & ( pre or post).cpp

--- Tree/Construct Tree from inorder & ( pre or post).cpp
+++ Tree/Construct Tree from inorder & ( pre or post).cpp
@@ -1,11 +1,11 @@
 Node* solve(vector<int>&in, vector<int>&pre, int & index,int inorderStart,int inorderEnd, int n,map<int,int>&m){
-        if(index>=n || inorderStart>inorderEnd) return NULL;  //In post : index<0
+        if(index>=n || inorderStart>inorderEnd) return NULL;  //In postorder : index<0
         int element=pre[index++];          //INpost :index--
         Node* root=new Node(element);
         int position=m[element];
         
-        root->left=solve(in, pre, index, inorderStart,position-1,n,m);  //INpost: postion+1,inorderEnd,n
-        root->right= solve(in,pre, index, position+1,inorderEnd,n,m);   //Inpost: inorderStart,post-1,n
+        root->left=solve(in, pre, index, inorderStart,position-1,n,m);  //In postorder: postion+1,inorderEnd,n
+        root->right= solve(in,pre, index, position+1,inorderEnd,n,m);   //In postorder: inorderStart,post-1,n
         return root;
 }
 
