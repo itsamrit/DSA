541125e1fe7f050cef069ba34753a60a0bae9731
Amrit Rai <amritrai9140@gmail.com>
2023-04-02 15:30:28 +0530

    Update Construct Tree from inorder & ( pre or post).cpp

--- Tree/Construct Tree from inorder & ( pre or post).cpp
+++ Tree/Construct Tree from inorder & ( pre or post).cpp
@@ -1,11 +1,13 @@
-Node* solve(vector<int>&in, vector<int>&pre, int & index,int inorderStart,int inorderEnd, int n,map<int,int>&m){
+//Prerequisite : inorder = left,root,right ,        pre= root,left,right          post = left,right,root
+
+Node* dfs(vector<int>&in, vector<int>&pre, int & index,int inorderStart,int inorderEnd, int n,map<int,int>&m){
         if(index>=n || inorderStart>inorderEnd) return NULL;  //In postorder : index<0
         int temp=pre[index++];          //INpost :index--
         Node* root=new Node(temp);
         int position=m[temp];
         
-        root->left=solve(in, pre, index, inorderStart,position-1,n,m);  //In postorder: postion+1,inorderEnd,n
-        root->right= solve(in,pre, index, position+1,inorderEnd,n,m);   //In postorder: inorderStart,post-1,n
+        root->left=dfs(in, pre, index, inorderStart,position-1,n,m);  //In postorder: postion+1,inorderEnd,n
+        root->right=dfs(in,pre, index, position+1,inorderEnd,n,m);   //In postorder: inorderStart,post-1,n
         return root;
 }
 
@@ -15,5 +17,5 @@ TreeNode* buildTree(vector<int>& pre, vector<int>& in) {
         map<int,int>m;          //Node to index mapping of inorder
         for(int i=0;i<n;i++)
                 m[in[i]]=i;
-        return solve(in,pre, 0,0,n-1,n,m);  //in postoder solve(in,post,n-1,rest same..) as index start from last in post
+        return dfs(in,pre, 0,0,n-1,n,m);  //in postoder solve(in,post,n-1,rest same..) as index start from last in post
 }
