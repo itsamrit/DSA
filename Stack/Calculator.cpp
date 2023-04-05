fa86e0665371b96d4fc13cf694cf2df8ee27918e
Amrit Rai <amritrai9140@gmail.com>
2023-04-05 12:46:22 +0530

    Update Calculator.cpp

--- Stack/Calculator.cpp
+++ Stack/Calculator.cpp
@@ -1,6 +1,8 @@
 🟢Like +- have same priority so as /* have same priority.eg: 2*3/2 .U either divide first or multiply ans will be same
 
+
 🟢You will be given either +-/* or +-() questions . You cant have */ and () combined and solved using stack
+   In both cases we need to make number from char so there will be another while loop inside main loop
    🟢IN +-/* whenever +- comes we simply push it into stack & whenever /* comes we go back or pop 1 element and push the current element multiplied or divided by poped element.
        At last we add all elemetns of stack.
        
