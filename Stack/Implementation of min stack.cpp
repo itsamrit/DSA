2592eacf60241d5ddb50e8ccd4b01d4a2a92b2d6
amritrai5757 <amritrai5757@gmail.com>
2023-04-22 09:33:04 +0530

    update

--- Stack/Implementation of min stack.cpp
+++ Stack/Implementation of min stack.cpp
@@ -1,14 +1,26 @@
 class MinStack {
 public:
-    stack<int> st1,st2;
-    void push(int val) {
-        if(st1.empty()){st1.push(val);st2.push(val);return ;}
-        st1.push(val); st2.push(min(val,st2.top()));
+    stack<int> st1;
+    stack<int> st2;
+    MinStack() {  // why nothing in constructor? because we are using stl stack so no need to initialize it
     }
+
+    void push(int x) {
+        st1.push(x);
+        if(st2.empty() || st2.top() >= x) st2.push(x);
+    }
+    
     void pop() {
-        if(st1.empty()){return ;}
-        st1.pop();st2.pop();
+        if(st1.top() == st2.top())
+            st2.pop();
+        st1.pop();
     }
-    int top(){ return st1.top();}
-    int getMin(){ return st2.top();}
+    
+    int top() {
+        return st1.top();
+    }
+    
+    int getMin() {
+        return st2.top();
+    }
 };
