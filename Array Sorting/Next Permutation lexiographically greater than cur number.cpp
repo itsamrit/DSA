868e62a32151a00a9b9958d34703f02c68ddd6bc
Amrit Rai <amritrai5757@gmail.com>
2023-05-18 19:38:28 +0530

    Update Next Permutation lexiographically greater than cur number.cpp

--- Array Sorting/Next Permutation lexiographically greater than cur number.cpp
+++ Array Sorting/Next Permutation lexiographically greater than cur number.cpp
@@ -2,24 +2,20 @@
 //Now from this place start iterating to last to find element which is closest greater than this. Swap both & sort the array from this place
 
 void nextPermutation(vector<int>& nums) {
-        int flag=0, maxx=nums.size()-1;
-        for(int i=nums.size()-1;i>=0;i--){
-            if(nums[i]<nums[maxx]){ 
-                for(int j=i+1;j<nums.size();j++){
-                    if(nums[i]<nums[j] && nums[maxx]>nums[j]){
-                        maxx=j;
+        for(int i=nums.size()-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                int j=i+1;
+                while(j<nums.size()){
+                    if(nums[j]<=nums[i]){
+                        break;
                     }
+                    j++;
                 }
-                swap(nums[i],nums[maxx]);
+                j--;
+                swap(nums[j],nums[i]);
                 sort(nums.begin()+i+1,nums.end());
-                flag=1;
-                break;
-                
+                return;
             }
-            if(nums[i]>nums[maxx])maxx=i;
         }
-        
-        if(flag==1)return;
         sort(nums.begin(),nums.end());
-        return;
 }
