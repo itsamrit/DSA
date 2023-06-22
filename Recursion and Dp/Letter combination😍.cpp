80a0f601e9c2069a5994a36953f11488b6cb87fc
Amrit Rai <amritrai5757@gmail.com>
2023-06-22 16:31:10 +0530

    Update Letter combination😍.cpp

--- Recursion and Dp/Letter combination😍.cpp
+++ Recursion and Dp/Letter combination😍.cpp
@@ -1,7 +1,7 @@
 //😍😍This problem is of 01 Combination of array of string of chars so its code looks like unbounded permutation but index is passed in dfs like we pass in combination
 //🟩🟩🟩🟩RATNA   ❤️❤️Since we're just adding a layer of loop in combination 01 algo to generate the strings, so our passing of 😍😍❤️❤️c is unaffected by loop's int i
 
-vector<string> mappings = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+vector<string> mapp = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 vector<string> ans;
 
 void dfs(string &digits, int c, string &temp){	
@@ -10,7 +10,7 @@ void dfs(string &digits, int c, string &temp){
 		return;
 	}
 
-	for(auto &i : mappings[digits[c] - '2']){
+	for(auto &i : mapp[digits[c] - '2']){
 		temp.push_back(i);
 		helper(digits, c + 1, temp);    // 😍Since its a combination problem we pass index c which is uneffected by for loop.
 		// ❤️❤️Since we're just adding a layer of loop in combination 01 algo to generate the strings, so our passing of 😍😍❤️❤️c is unaffected by loop's int i
