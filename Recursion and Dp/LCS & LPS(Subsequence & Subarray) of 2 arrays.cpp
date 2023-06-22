0c6a736ddf96e512151f8184dc34084836bb1283
Amrit Rai <amritrai5757@gmail.com>
2023-06-22 10:32:11 +0530

    Update LCS & LPS(Subsequence & Subarray) of 2 arrays.cpp

--- Recursion and Dp/LCS & LPS(Subsequence & Subarray) of 2 arrays.cpp
+++ Recursion and Dp/LCS & LPS(Subsequence & Subarray) of 2 arrays.cpp
@@ -65,18 +65,10 @@ while(i>0 && j>0){
 reverse(s.begin(),s.end());
 
 
+
 // Shortest common superseq i,e shortest sequence from which we can obtain both x & y by deleting some element = x.size()+y.size()-longest common subsequence
-// Min no of insertion and deletion to make string x to string y :  deletion(x.size()-lcs)  +  insertion(y.size()-lcs)   lcs:longest common subsequence
-// Min no insertion and deletion and replacement to make string x to y:EDIT DISTANCE  for..{for..
-                                                                                         //if(i==0)dp[i][j]=j; 
-                                                                                         //else if(j==0)dp[i][j]=i;    
-                                                                                         //else if(x[i-1]==y[i-1])dp[i][j]=dp[i-1][j-1];
-                                                                                         //else dp[i][j]= 1+ min(dp[i][j-1]insert,min(dp[i-1][j]remove,dp[i-1][j-1]replace));
 // LPS:Longest palindromic subsequence of an arr : x=a & y=reverse(a);  lcs(x,y)    Follow up que: Min no of deletion to make array palindrom: arr.size()-lps
 // Min no insertion and deletion to make string palindrom : no of insertion = no of deletion = s.size()-lps(s)
 // LRS:Longest repeting subsequence of an arr : aabbdd =3 abd coming 2times: x=arr,y=arr; lcs(x,y) with restriction if(X[i-1]==Y[j-1] && i!=j).....
 // Check if x is a subsequence of y : if(lcs(x,y)==x.size()) cout<<"yes"; 
-// EDIT DISTANCE : Min insertion/deletion/replacement in string a to make it b. If u insert in something in a it will match a char of b so u need to check only f(i,j-1(b))
-                                                                                If u delete something in a f(i-1,j)
-                                                                                If u replace something in a with b f(i-1,j-1)
-
+// In problems like min insertion, deletion to make string x to y , U dont actually insert, delete or replace a char, u just assume char inserted so move to next char
