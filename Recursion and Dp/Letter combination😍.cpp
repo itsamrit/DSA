82690191601fcad92cc61b10366eb9807c7f2244
Amrit Rai <amritrai5757@gmail.com>
2023-06-22 16:10:34 +0530

    Update Letter combination😍.cpp

--- Recursion and Dp/Letter combination😍.cpp
+++ Recursion and Dp/Letter combination😍.cpp
@@ -1,15 +1,15 @@
-//😍😍This problem is of 01 Combination of array of string of chars so its code looks like 01 permutation
+//😍😍This problem is of 01 Combination of array of string of chars so its code looks like unbounded permutation but index is passed in dfs i like we pass in combination
 //🟩🟩🟩🟩RATNA
 vector<string> mappings = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 vector<string> ans;
 
 vector<string> letterCombinations(string digits) {
 	if(digits == "") return ans;         // no combinations formed without any digits
-	helper(digits, 0, "");              
+	dfs(digits, 0, "");              
 	return ans;
 }
 
-void helper(string &digits, int i, string combi){	
+void dfs(string &digits, int i, string combi){	
 	if(i == size(digits)) { 
 		ans.push_back(combi);    
 		return;
@@ -17,7 +17,7 @@ void helper(string &digits, int i, string combi){
 
 	for(auto &c : mappings[digits[i] - '2']){
 		combi.push_back(c);
-		helper(digits, i + 1, combi);
+		helper(digits, i + 1, combi);    // 😍Since its a combination problem we pass index i
 		combi.pop_back();
 	}
 }
