6858084f8ae57a7edbd782f9deef631a8e90f23e
Amrit Rai <amritrai5757@gmail.com>
2023-06-23 17:52:18 +0530

    Update and rename 🟥🟥Matrix's Permutations to place things in it.cpp to Matrix's Permutations to place things in it.cpp

--- Recursion and Dp/Matrix's Permutations to place things in it.cpp
+++ Recursion and Dp/Matrix's Permutations to place things in it.cpp
@@ -1,7 +1,50 @@
-// 🟥🟥TOO LONG TO COME IN INTERVIEW.
-// 🟥🟥TOO LONG TO COME IN INTERVIEW.
 // ALL QUESTIONS ARE BOUNED unlike unbounded subset & on SQUARE MATRIX
 
+// Sudoku Solver :  🟩Since the sequence of numbers in sudoku matters , its a permutation problem
+
+bool safe(vector<vector<char>> board,int i,int j,char k){
+    for(int r=0;r<9;r++)
+      if((board[r][j]==k)&&i!=r) return false;
+    for(int r=0;r<9;r++)
+      if((board[i][r]==k)&&j!=r) return false;
+    for(int r=0;r<3;r++)
+    for(int c=0;c<3;c++)
+    if((board[(i/3)*3+r][(j/3)*3+c]==k)&&(i!=(i/3)*3+r)&&(j!=(j/3)*3+c))      
+      return false;
+      return true;
+}
+
+
+bool dfs(vector<vector<char>>& board){
+    
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.')
+                 { for(char k='1';k<='9';k++)
+                    { 
+                        if(safe(board,i,j,k)){
+                            board[i][j]=k;
+                            if(dfs(board))          // 😍😍Since its permutation were not passing current row or col in dfs
+                              return true;
+                              board[i][j]='.';
+                              
+                        }
+                    }
+                    
+                 } 
+                 if(board[i][j]=='.') return false;  //backtracking
+            }
+            
+        }
+       return true;
+}
+
+
+
+
+
+// 🟥🟥TOO LONG TO COME IN INTERVIEW.
+// 🟥🟥TOO LONG TO COME IN INTERVIEW.
 //CROSSWORD PUZZLE : A matrix is provided with + - sign and there is vector of strings which need to filled in matrix. Only - can be filled. eg : car should remain car while filling it cant be filled in arc.
 
 vector<string> checkHorizontal(int x, int y, vector<string> matrix, string currentWord){
