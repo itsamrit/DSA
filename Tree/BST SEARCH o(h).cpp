bcb72377017d4b8845d868c175a7d0b0fda78779
Amrit Rai <amritrai5757@gmail.com>
2023-06-28 13:13:52 +0530

    Update BST SEARCH o(h).cpp

--- Tree/BST SEARCH o(h).cpp
+++ Tree/BST SEARCH o(h).cpp
@@ -13,3 +13,24 @@ TreeNode* searchBST(TreeNode* root, int val) {
 }
 
 
+
+
+
+
+
+Q; Given a bst .Find succesor of given element according to its inorder traversal.
+//Searching : tc:o(h)  sc: o(1) 
+TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        TreeNode* successor = NULL;
+        while (root) {
+            if (p->val >= root->val)
+                root = root->right;
+            else {
+                successor = root;
+                root = root->left;
+            }
+        }
+        return successor;
+}
+
+
