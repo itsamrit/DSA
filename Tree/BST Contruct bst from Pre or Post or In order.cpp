603bb469d2bfee9a10d654ee209278b1b5f79621
Amrit Rai <amritrai5757@gmail.com>
2023-06-28 13:34:46 +0530

    Update BST Contruct bst from Pre or Post or In order.cpp

--- Tree/BST Contruct bst from Pre or Post or In order.cpp
+++ Tree/BST Contruct bst from Pre or Post or In order.cpp
@@ -16,23 +16,3 @@ TreeNode* bstFromPreorder(vector<int>& A) {
         int i = 0;                                     //In postorder : i=A.size()-1;
         return dfs(A, i, INT_MAX);                     //IN postorder : INT_MIN
 }
-
-
-// Construct HEIGHt balanced tree from inorder Linked List i,e sorted linked list :-    
-TreeNode* sortedListToBST(ListNode* head) {
-        if(!head)  return NULL;
-        if(!head->next) return new TreeNode(head->val);
-        
-        ListNode *slow=head,*fast=head->next;
-        while(fast && fast->next&&fast->next->next){
-            slow=slow->next;
-            fast=fast->next->next;   
-        }
-        ListNode *mid=slow->next;  
-        slow->next=NULL;                          // Breaking the linkedlist into 2 equal halves
-        
-        TreeNode *root=new TreeNode(mid->val);
-        root->left=sortedListToBST(head);
-        root->right=sortedListToBST(mid->next);
-        return root;
-}
