0c1842ce0cf09f0ecf0b27162a5b4e1ecb143186
Amrit Rai <amritrai5757@gmail.com>
2023-06-28 09:18:59 +0530

    Update BST Contruct bst from Pre or Post or In order.cpp

--- Tree/BST Contruct bst from Pre or Post or In order.cpp
+++ Tree/BST Contruct bst from Pre or Post or In order.cpp
@@ -2,18 +2,19 @@
 // Tc : o(n) SC; o(n) auxiliary space.Use this as striver has use recursive solution
 // Use intuitive/non recursive solution only in searching in bst
 // No questions on Inorder to bst 
-
-TreeNode* build(vector<int>& A, int& i, int bound) {
-        if (i == A.size() || A[i] > bound) return NULL;// In postorder : i==-1 || A[i]<bound  // 🟩root->val=NULL means root->val=0 not null . So dont return root->val =NULL, return NULL;
-        TreeNode* root = new TreeNode(A[i++]);         // In postorder : A[i--]
-        root->left = build(A, i, root->val);
-        root->right = build(A, i, bound);
+// 🟩root->val=NULL means root->val=0 not null . So dont return root->val =NULL, return NULL
+        
+TreeNode* dfs(vector<int>& A, int& i, int bound) {
+        if (i == A.size() || A[i] > bound) return NULL; // In postorder : i==-1 || A[i]<bound  // 🟩root->val=NULL means root->val=0 not null . So dont return root->val =NULL, return NULL;
+        TreeNode* root = new TreeNode(A[i++]);          // In postorder : A[i--]
+        root->left = dfs(A, i, root->val);
+        root->right = dfs(A, i, bound);
         return root;
 }
 
 TreeNode* bstFromPreorder(vector<int>& A) {
         int i = 0;                                     //In postorder : i=A.size()-1;
-        return build(A, i, INT_MAX);                   //IN postorder : INT_MIN
+        return dfs(A, i, INT_MAX);                     //IN postorder : INT_MIN
 }
 
 
