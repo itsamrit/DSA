7bf9cb0b72f01a4f53214ef28713bcbac464aab5
Amrit Rai <amritrai5757@gmail.com>
2023-07-13 08:09:29 +0530

    Update Peak Element in Unsorted Array.cpp

--- Binary Search/Peak Element in Unsorted Array.cpp
+++ Binary Search/Peak Element in Unsorted Array.cpp
@@ -1,6 +1,7 @@
 int findPeakElement(vector<int>& nums) {
         int left=0,right=nums.size()-1;
         if(nums.size()==1)return 0;
+        
         while(left<=right){
             int mid=left+(right-left)/2;
             if((mid==0 && nums[mid]>nums[mid+1]) || (mid==nums.size()-1 && nums[mid]>nums[mid-1])){
