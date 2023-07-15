664471adce9c61ae2edeb1b278f5d15beb7e95a0
Amrit Rai <amritrai5757@gmail.com>
2023-07-15 16:17:45 +0530

    Update Compare if Symmetric or Identical trees.cpp

--- Tree/Compare if Symmetric or Identical trees.cpp
+++ Tree/Compare if Symmetric or Identical trees.cpp
@@ -20,26 +20,17 @@ bool identical (TreeNode* p, TreeNode* q) {
                                   // For symmetricity : if(!(root->left) && !(root->right))return true;
                                   // For symmetricity : if(!(root->left) || !(root->right))return false;
         ans=true;
+                                  
         dfs(p,q);
         return ans;
 }
 
 
 
-//🟩o(n^2) compare if subtree is part a tree leaves must be same 
-    bool dfs(TreeNode* root, TreeNode* sub){
-        if(!root && !sub)return true;
-        if(!root || !sub )return false;
-        if(root->val != sub->val)return false;
-        bool left=dfs(root->left,sub->left);
-        bool right=dfs(root->right,sub->right);
-        if(left && right)return true;
-        return false;
-    }
-    
-    bool isSubtree(TreeNode* root, TreeNode* sub) {
+//🟩compare if subtree is part a tree leaves must be same : Check identical for each node of tree with subtree. tc = o(n^2) 
+bool isSubtree(TreeNode* root, TreeNode* sub) {
         if(!root && !sub)return true;
         if(!root || !sub)return false;
         if(dfs(root,sub) || isSubtree(root->left, sub) || isSubtree(root->right,sub))return true;
         return false;
-    }
+}
