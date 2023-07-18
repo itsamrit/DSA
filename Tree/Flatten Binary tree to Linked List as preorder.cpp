9990c072652edcc0bebf427966f5bb2d5d0aa3da
Amrit Rai <amritrai5757@gmail.com>
2023-07-18 08:02:09 +0530

    Update Flatten Binary tree to Linked List as preorder.cpp

--- Tree/Flatten Binary tree to Linked List as preorder.cpp
+++ Tree/Flatten Binary tree to Linked List as preorder.cpp
@@ -4,10 +4,10 @@ void dfs(TreeNode* root) {
         dfs(root->right);
         
         if(root->left){  // Change links : make left subtree attached with root->right & then new root->right with previous root->right  as preorder : root->left->right
-            TreeNode *right = root->right;
+            TreeNode *temp = root->right;
             root->right = root->left;
             root->left = NULL;             //🟩Dont forget to make left NULL,otherwise error occurs
             while(root->right) root = root->right;
-            root->right = right;
+            root->right = temp;
         }        
 }
