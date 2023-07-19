bc866611ddb0f5c1718db3bc332a003494825459
Amrit Rai <amritrai5757@gmail.com>
2023-07-19 14:19:18 +0530

    Update Traversal Verticalorder⭐.cpp

--- Tree/Traversal Verticalorder⭐.cpp
+++ Tree/Traversal Verticalorder⭐.cpp
@@ -9,16 +9,16 @@ vector<vector<int>> verticalTraversal(Node* root){
         q.push({root, 0});                   // q.push(node,col no); We assume root as col no 0 & all left will become negative col no as ...-3, -2, -1, (root)0, 1,2 3 ...
         while(q.size()){
             int sz = q.size();
-            vector<pair<int, int>>a; 
+            vector<pair<int, int>>temp; 
             for(int i=0;i<sz;i++){
-                pair<Node*, int> temp = q.front();
+                pair<Node*, int> c = q.front();
                 q.pop();
-                a.push_back({temp.second, temp.first->val});
-                if(temp.first->left)q.push({temp.first->left, temp.second - 1});
-                if(temp.first->right)q.push({temp.first->right, temp.second + 1});
+                temp.push_back({c.second, c.first->val});
+                if(c.first->left) q.push({c.first->left,  c.second - 1});
+                if(c.first->right) q.push({c.first->right,c.second + 1});
             }
-            sort(a.begin(),a.end());                       //if 5,6 are in same col & same row i,e in same cell so 5 comes first
-            for(auto it:a)m[it.first].push_back(it.second);   
+            sort(temp.begin(),temp.end());                       //if 5,6 are in same col & same row i,e in same cell so 5 comes first
+            for(auto it:temp)m[it.first].push_back(it.second);   
         }
         for(auto it:m)ans.push_back(it.second);             //CANT USE UNORDERED_MAP since we want col no to be sorted
         return ans; 
