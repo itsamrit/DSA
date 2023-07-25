5e39ed30c174b38684ea28bf874d11bfb9a7f766
Amrit Rai <amritrai5757@gmail.com>
2023-07-25 09:38:08 +0530

    Update Total nodes in complete binary tree.cpp

--- Tree/Total nodes in complete binary tree.cpp
+++ Tree/Total nodes in complete binary tree.cpp
@@ -1,6 +1,6 @@
 Tc: 0(height) = o(logn)
 
-int findHeightLeft(TreeNode* node) {
+int LeftHeight(TreeNode* node) {
         int hght = 0; 
         while(node) {
             hght++; 
@@ -9,7 +9,7 @@ int findHeightLeft(TreeNode* node) {
         return hght; 
 }
 
-int findHeightRight(TreeNode* node) {
+int RightHeight(TreeNode* node) {
         int hght = 0; 
         while(node) {
             hght++; 
@@ -21,8 +21,8 @@ int findHeightRight(TreeNode* node) {
 int countNodes(TreeNode* root) {
         if(!root) return 0; 
         
-        int lh = findHeightLeft(root); 
-        int rh = findHeightRight(root); 
+        int lh = LeftHeight(root); 
+        int rh = RightHeight(root); 
         
         if(lh == rh) return pow(2,lh)-1;   //2^height-1          //In bits u can return (1<<lh) - 1; 
         
