0b7a8a356690597688b27fe9417a476aecc2d389
Amrit Rai <amritrai5757@gmail.com>
2023-08-12 19:39:38 +0530

    Update and rename Famous Problems to Word Break.cpp

--- Recursion and Dp/Word Break.cpp
+++ Recursion and Dp/Word Break.cpp
@@ -1,5 +1,8 @@
-Word Break : More similar problems to word break : Extra Characters in a String
-Method 2 : You just need to match all words with all dictionary. No optimization .  So instead of searching each string in dictionary , you can do vice versa
+🟩🟩U need to cross match all words of dictionary to all words formed from string 
+// More similar problems to word break : Extra Characters in a String
+// Method 2 : You just need to match all words with all dictionary.  So instead of searching each string in dictionary , you can do vice versa.
+//     🟩In dp there is no optimization that u start from smallest or largest word. Start from anywhere, u need to cross match all words of dictionary to all words formed from string and find min or max or whatever is asked.
+    
     set<string>st;
     vector<int>dp;
     int dfs(string &s,int c){
