7e27a1269b465cd8231cab78f195079f5acb9e20
Amrit Rai <amritrai5757@gmail.com>
2023-10-05 23:55:28 +0530

    Update Maths Subarray with sum product exactly k(Prefix sum).cpp

--- Subarray/Maths Subarray with sum product exactly k(Prefix sum).cpp
+++ Subarray/Maths Subarray with sum product exactly k(Prefix sum).cpp
@@ -1,6 +1,6 @@
 // ✅✅If subarray can become valid(according to problem) again by adding more characters ,i,e, we cant use 2 pointer(sliding window) i,e cant move left pointer as addition of more charaters can make it valid again
 // ✅ eg : target =4 and sliding window got sum 6 but still we cant move left pointer since a negative no may come -2 and sum become from current i to -2 as 4
-// ✅Except SUM QUESTIONS AND VARIABLE SLIING WINDOW are takes o(n^2) time
+// ❤️❤️❤️EXCEPTION SUM QUESTIONS AND VARIABLE SLIING WINDOW are takes o(n^2) time
 // ✅✅✅IF ASKED VARIABLE SIZE AND SEMI VARIABLE i,e greater than or smaller than given value with all positive elements in array then use sliding window VARIABLE SUBARRAY  
 //        eg: Smallest subarray with sum greater than or equal to x
 // ✅MATHS questions which cant be solved by sliding can be solved by this prefix sum. Like subarray with sum 0 with neg elements
