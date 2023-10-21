a20862b5cdfba60a7b49f60aeba63c3b9e256996
Amrit Rai <amritrai5757@gmail.com>
2023-10-22 02:55:42 +0530

    Update and rename Desing rating sytem to Desing rating sytem.cpp

--- Design/Desing rating sytem.cpp
+++ Design/Desing rating sytem.cpp
@@ -5,5 +5,11 @@ Always use map instead of set and priority queue all have same time complexity
 
 set is use to just find whether present or not or to sort but both these can be performed by map .
 map<int, set<pair<int, string>>> m; // used to find lowest or highest sorted 
+
 map<int , map<int,string>>m;
 use rbegin and rend to use descend ing order
+
+👌When we have multiple same key and we want to search , delete a a thing and we know both key and value then,
+  Since even after finding key we need to loop to search for particular value .
+We use set<pair<key,value>>   or map<pair<key,value>, useless>>  not map<key,value> 
+becaue we search in logn m.count({key,value})   m.erase({key,value})
