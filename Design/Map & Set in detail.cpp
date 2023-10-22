5a501333fb139b94c44a8fdf5908d1e7216a0a46
Amrit Rai <amritrai5757@gmail.com>
2023-10-22 14:19:20 +0530

    Update and rename Desing rating sytem.cpp to Map & Set in detail.cpp

--- Design/Map & Set in detail.cpp
+++ Design/Map & Set in detail.cpp
@@ -1,6 +1,6 @@
 MUST DO QUESTION TO UNDERSTAND NESTED MAP,SET, and priority queue
 map<int,int> is actually set<pair<int,int>> so tc doesnt effected even if u use map 
-https://leetcode.com/problems/design-a-food-rating-system/description/
+
 Always use map instead of set and priority queue all have same time complexity 
 
 set is use to just find whether present or not or to sort but both these can be performed by map .
@@ -16,3 +16,45 @@ becaue we search in logn m.count({key,value})   m.erase({key,value})
 
 
 💕Multiset or multimap has 1 problem u need another internal serach since there can be multiple same key
+
+
+Design rating system
+https://leetcode.com/problems/design-a-food-rating-system/description/
+
+
+
+
+
+
+Design number container system
+https://leetcode.com/problems/design-a-number-container-system/
+
+class NumberContainers {
+public:
+
+    map<int,int>m1; // index, number
+    map<int,set<int>>m2;// number, indexes
+
+    NumberContainers() {
+    }
+    
+    void change(int index, int number) {
+        if(m1.find(index)!=m1.end()){
+            int temp= m1[index];
+            m2[temp].erase(index);
+            if(m2[temp].size()==0)m2.erase(temp);
+            m1[index]=number;
+            m2[number].insert(index);
+        }
+        else{
+            m1[index]=number;
+            m2[number].insert(index);
+        }
+    }
+    
+    int find(int number) {
+        if(m2.count(number))
+        return *m2[number].begin();
+        else return -1;
+    }
+};
